fc5046ff203e760ab1d76908a0e3082b2b948679
thien19dev <thien19dev@gmail.com>
2024-09-27 15:52:56 +0700

    Add server.js for deployment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const Car = require('../model/car');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+// Them Du Lieu Vao DataBase
+router().post('/addCar', (req, res, next) => {
+  const data = req.body;
+  const newCar = new Car({
+    maXe: data.maXe,
+    mauSac: data.mauSac,
+    giaTien: data.giaTien
+  });
+
+  newCar.save()
+      .then(() =>{
+        res.send("Them thanh cong!")
+      }).catch(err => {
+        res.send("Them that bai" + err);
+  });
 });
 
 module.exports = router;
