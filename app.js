ef724fd1fe24c55c041246848486acd22ac3ec45
thien19dev <thien19dev@gmail.com>
2024-09-27 20:36:20 +0700

    commit link mongooDB

--- app.js
+++ app.js
@@ -43,4 +43,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
